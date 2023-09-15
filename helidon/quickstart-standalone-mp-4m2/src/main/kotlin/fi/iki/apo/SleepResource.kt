38ad3c24aa8ff8bcb44667ef54e49f43a4a74038
Mikko Apo <mikko.apo@reaktor.fi>
2023-09-15 09:37:56 +0300

    jvm21-demo: converted SleepResource to Kotlin

--- helidon/quickstart-standalone-mp-4m2/src/main/kotlin/fi/iki/apo/SleepResource.kt
+++ helidon/quickstart-standalone-mp-4m2/src/main/kotlin/fi/iki/apo/SleepResource.kt
@@ -1,32 +1,26 @@
 package fi.iki.apo;
 
-import jakarta.enterprise.context.RequestScoped;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.core.Response;
-
-import java.time.Duration;
+import jakarta.enterprise.context.RequestScoped
+import jakarta.ws.rs.GET
+import jakarta.ws.rs.Path
+import jakarta.ws.rs.PathParam
+import jakarta.ws.rs.core.Response
+import java.time.Duration
 
 @Path("/sleep")
 @RequestScoped
-public class SleepResource {
-
+class SleepResource {
     /**
      * Sleep x seconds
      *
      * @param sleepSeconds JSON containing the new greeting
-     * @return {@link Response}
+     * @return [Response]
      */
     @Path("{sleepSeconds}")
     @GET
-    public String sleep(@PathParam("sleepSeconds") long sleepSeconds) {
-        try {
-            Thread.sleep(Duration.ofSeconds(sleepSeconds));
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        return "Slept " + sleepSeconds + " seconds!";
+    fun sleep(@PathParam("sleepSeconds") sleepSeconds: Long): String {
+        Thread.sleep(Duration.ofSeconds(sleepSeconds))
+        return "Slept $sleepSeconds seconds!"
     }
-
 }
+
